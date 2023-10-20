11632155e4db287d47c4966dad81bba569704e4d
kimdev <kimdev1242@gmail.com>
2023-10-20 22:33:16 +0900

    first commit

---
+++ src/test/java/com/practice/pass_web/PassWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pass_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
